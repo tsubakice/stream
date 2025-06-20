fdde1969598e1ba8a1984068f47cf01a730e2cae
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-06-20 14:59:26 +0800

    自定义元素处理

--- reactor/src/main/java/org/qiaice/App.java
+++ reactor/src/main/java/org/qiaice/App.java
@@ -1,42 +1,13 @@
 package org.qiaice;
 
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class App {
     public static void main(String[] args) {
 
-        // 编程式创建 Flux 流 -- 同步环境下
-        Flux.generate(AtomicInteger::new, (state, sink) -> {
-            if (state.getAndIncrement() >= 10) {
-                sink.complete();
-            } else {
-                sink.next(state);
-            }
-            return state;
-        }).log().limitRate(5).subscribe();
-
-        class UserLoginListener {
-            final FluxSink<Object> sink;
-
-            UserLoginListener(FluxSink<Object> sink) {
-                this.sink = sink;
-            }
-
-            void login(String username) {
-                sink.next(username);
-            }
-        }
-
-        // 编程式创建 Flux 流 -- 异步环境下
-        // 官方将其与监听器相结合, 达到事件触发后向通道中添加元素的效果
-        Flux.create(sink -> {
-            UserLoginListener listener = new UserLoginListener(sink);
-            for (int i = 0; i < 10; i++) {
-                listener.login("用户-" + i);
-            }
-        }).log().limitRate(5).subscribe();
+        // 自定义元素处理, 相当于更强大的 map
+        Flux.range(1, 10)
+                .handle((e, sink) -> sink.next(e * e))
+                .log().subscribe();
     }
 }
