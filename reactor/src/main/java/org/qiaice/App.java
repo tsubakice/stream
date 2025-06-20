24d967318ba62ad6b42ebbe0171821f576c2ef84
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-06-20 14:04:31 +0800

    buffer 和 limitRate 等运算符的简单使用

--- reactor/src/main/java/org/qiaice/App.java
+++ reactor/src/main/java/org/qiaice/App.java
@@ -1,49 +1,15 @@
 package org.qiaice;
 
-import org.reactivestreams.Subscription;
-import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.SignalType;
 
 public class App {
     public static void main(String[] args) {
-        Flux.range(0, 10)
-                .subscribe(new BaseSubscriber<>() {
+        Flux<Integer> flux = Flux.range(0, 10);
 
-                    @Override
-                    protected void hookOnSubscribe(Subscription subscription) {
-                        System.out.println("订阅成功");
-                        request(1);
-                    }
+        // 请求重塑 buffer, 将流中的数据按个数封装为 List
+        flux.buffer(4).log().subscribe();
 
-                    @Override
-                    protected void hookOnNext(Integer value) {
-                        System.out.println("订阅到达: " + value);
-                        if (value == 5) {
-                            throw new RuntimeException("exception");
-                        }
-                        request(1);
-                    }
-
-                    @Override
-                    protected void hookOnComplete() {
-                        System.out.println("订阅完成");
-                    }
-
-                    @Override
-                    protected void hookOnError(Throwable throwable) {
-                        System.out.println("订阅异常");
-                    }
-
-                    @Override
-                    protected void hookOnCancel() {
-                        System.out.println("订阅被取消");
-                    }
-
-                    @Override
-                    protected void hookFinally(SignalType type) {
-                        System.out.println("订阅结束");
-                    }
-                });
+        // 请求重塑 limitRate, 限制请求数据的速率
+        flux.log().limitRate(5).subscribe();
     }
 }
