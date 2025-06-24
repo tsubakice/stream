373b85b49275595c858bf6837b3717604f62f5ba
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-06-24 15:38:48 +0800

    超时与重试机制

--- reactor/src/test/java/org/qiaice/AppTest.java
+++ reactor/src/test/java/org/qiaice/AppTest.java
@@ -3,21 +3,18 @@ package org.qiaice;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
 class AppTest {
 
-    // 自定义异常处理
-    // onErrorReturn: 返回自定义值
-    // onErrorResume: 在返回自定义值之前进行额外的操作
-    // onErrorComplete: 将异常信号转换为完成信号
-    // onErrorMap: 将异常包装为自定义异常, 并向下传递
-    // onErrorContinue: 处理异常后继续推进
-    // onErrorStop: 发生异常后停止流(从根本上的停止, 也就是会停止所有监听者的监听)
+    // 重试不止单单请求导致重试的元素, 而是重新请求这条流
 
     @Test
-    void errorTest() {
-        Flux.just(1, 2, 0, 3)
-                .map(element -> 100 / element)
-                .onErrorContinue((t, e) -> System.out.println("元素: " + e + " 存在异常: " + t.getLocalizedMessage()))
-                .subscribe(e -> System.out.println("e -> " + e), System.err::println);
+    void timeoutAndRetryTest() {
+        Flux.just(0, 1, 2, 3, 4)
+                .delayElements(Duration.ofSeconds(2))
+                .timeout(Duration.ofSeconds(1)) // 设置超时时间
+                .retry(3) // 设置重试次数
+                .log().subscribe();
     }
 }
