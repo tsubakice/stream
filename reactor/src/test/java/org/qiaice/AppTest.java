61a14cd8a73a73fd1dc63103e99df0138fa85a1e
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-06-24 15:27:40 +0800

    自定义异常处理

--- reactor/src/test/java/org/qiaice/AppTest.java
+++ reactor/src/test/java/org/qiaice/AppTest.java
@@ -2,51 +2,22 @@ package org.qiaice;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 class AppTest {
 
-    @Test
-    void filterTest() {
-        Flux.range(0, 10)
-                .filter(element -> element % 2 == 0)
-                .log().subscribe();
-    }
+    // 自定义异常处理
+    // onErrorReturn: 返回自定义值
+    // onErrorResume: 在返回自定义值之前进行额外的操作
+    // onErrorComplete: 将异常信号转换为完成信号
+    // onErrorMap: 将异常包装为自定义异常, 并向下传递
+    // onErrorContinue: 处理异常后继续推进
+    // onErrorStop: 发生异常后停止流(从根本上的停止, 也就是会停止所有监听者的监听)
 
     @Test
-    void flatMapTest() {
-        Flux.just("1-2", "3-4", "5-6")
-                .flatMap(element -> Flux.fromArray(element.split("-")))
-                .log().subscribe();
-    }
-
-    @Test
-    void concatTest() {
-        Flux.concat(Flux.range(0, 5), Flux.range(5, 5))
-                .log().subscribe();
-    }
-
-    // transform 与 transformDeferred 的区别就是是否重复计算
-    // transform 仅计算一次转换逻辑, 并将该逻辑应用于所有订阅者
-    // transformDeferred 每次订阅时都会重新计算转化逻辑, 适用于为每个订阅者提供独立状态的场景
-    @Test
-    void transformTest() {
-        Flux<Integer> flux1 = Flux.range(0, 10)
-                .transform(flux -> flux.map(element -> element * 2)
-                        .filter(element -> element > 5));
-        flux1.log().subscribe();
-        flux1.log().subscribe();
-    }
-
-    @Test
-    void emptyTest() {
-        Mono.empty().defaultIfEmpty("default").log().subscribe();
-        Mono.empty().switchIfEmpty(Mono.just("switch")).log().subscribe();
-    }
-
-    @Test
-    void zipTest() {
-        Flux.zip(Flux.range(0, 5), Flux.range(5, 5))
-                .log().subscribe();
+    void errorTest() {
+        Flux.just(1, 2, 0, 3)
+                .map(element -> 100 / element)
+                .onErrorContinue((t, e) -> System.out.println("元素: " + e + " 存在异常: " + t.getLocalizedMessage()))
+                .subscribe(e -> System.out.println("e -> " + e), System.err::println);
     }
 }
